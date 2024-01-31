f067434558f9ac5ce18c80e30a23becd7615d274
jakhong1r-dev <jahongirabduazimov707@gmail.com>
2024-01-31 22:24:18 +0500

    Dinamiklashtirildi

---
+++ main.js
@@ -0,0 +1,55 @@
+"use strict"
+
+let mostSalesWrapper = document.querySelector(".most-sales__wrapper");
+
+for(let i = 1; i <= 10; i++) {
+    let mostSalesCard = document.createElement('div');
+
+    mostSalesCard.setAttribute('class', 'most-sales w-[230px] h-[377px] bg-white rounded-[10px] p-[10px] relative')
+
+    mostSalesCard.innerHTML = `
+    <img class="mx-auto" src="./assets/images/laptop2.png" alt="img">
+    <img class="absolute top-[10px] right-[10px] cursor-pointer" src="./assets/images/favorite_border.svg" alt="icon">
+    <div class="pl-[10px]">
+        <div>
+            <p class="text-[#0D63F3] text-[18px]">299 000 so’m</p>
+            <p class="text-[14px] leading-4">Apple Airpods Pro simsiz quloqchin , Oq rangda   </p>
+        </div>
+        <div class="absolute bottom-[10px]">
+            <p class="text-[14px] text-[#6C757D] mb-[25px]">1230 ta buyurtma</p>
+            <div class="flex gap-x-[28px]">
+                <button class="w-[130px] py-[10px] bg-[#0D63F3] text-white rounded-lg">Sotib olish</button>
+                <img class="cursor-pointer" src="./assets/images/shopping_cart.svg" alt="icon">
+            </div>
+        </div>
+    </div>
+    `
+    mostSalesWrapper.append(mostSalesCard)
+}
+
+let mostPopulationWrapper = document.querySelector(".most-population__wrapper");
+
+for(let i = 1; i <= 10; i++) {
+    let mostSalesCard = document.createElement('div');
+
+    mostSalesCard.setAttribute('class', 'most-sales w-[230px] h-[377px] bg-white rounded-[10px] p-[10px] relative')
+
+    mostSalesCard.innerHTML = `
+    <img class="mx-auto" src="./assets/images/laptop2.png" alt="img">
+    <img class="absolute top-[10px] right-[10px] cursor-pointer" src="./assets/images/favorite_border.svg" alt="icon">
+    <div class="pl-[10px]">
+        <div>
+            <p class="text-[#0D63F3] text-[18px]">299 000 so’m</p>
+            <p class="text-[14px] leading-4">Apple Airpods Pro simsiz quloqchin , Oq rangda   </p>
+        </div>
+        <div class="absolute bottom-[10px]">
+            <p class="text-[14px] text-[#6C757D] mb-[25px]">1230 ta buyurtma</p>
+            <div class="flex gap-x-[28px]">
+                <button class="w-[130px] py-[10px] bg-[#0D63F3] text-white rounded-lg">Sotib olish</button>
+                <img class="cursor-pointer" src="./assets/images/shopping_cart.svg" alt="icon">
+            </div>
+        </div>
+    </div>
+    `
+    mostPopulationWrapper.append(mostSalesCard)
+}
